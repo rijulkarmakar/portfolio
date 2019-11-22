f720455c7ed93059bf92a1aa4e095190fdc91516
rijulkarmakar <rijul2012.007@gmail.com>
2019-11-22 20:59:36 +0530

    Blog Model Added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    blogNo=models.IntegerField()
+    title = models.CharField(max_length=20)
+    publicationDate=models.DateTimeField()
+    body=models.TextField()
+    image=models.ImageField()
